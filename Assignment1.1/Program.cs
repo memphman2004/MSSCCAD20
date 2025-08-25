1a172e200409981f69f968ae6e5344edb0a8933e
memphman2004 <memphman2004@yahoo.com>
2025-08-25 16:04:05 -0400

    Add project files.

---
+++ Assignment1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+        string name;
+        int age;
+        string address;
+    Console.Write("Enter you name: ");
+    name = Console.ReadLine();
+
+    Console.Write("Enter your age: ");
+    age = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter your address: ");
+    address = Console.ReadLine();
+
+    Console.WriteLine($"Hello, {name}. You are {age} years old and live at {address}.");
+
+}
---
+++ Assignment1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+    int num1;
+    int num2;
+    int sum;
+
+    Console.WriteLine("Enter your first number: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter your second number: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    sum = num1 + num2;
+
+    Console.WriteLine($"The sum of {num1} and {num2} is: {sum}.");
+
+}
