875bae37c194fec9aaa1a7779d4191e711ef20e3
memphman2004 <memphman2004@yahoo.com>
2025-09-18 17:54:44 -0400

    update

---
+++ CalculatorApp/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorApp
+{
+    public interface ICalculator
+    {
+        decimal Add(decimal a, decimal b);
+        decimal Subtract(decimal a, decimal b);
+        decimal Multiply(decimal a, decimal b);
+        decimal Divide(decimal a, decimal b);
+    }
+}
---
+++ CalculatorApp/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Math : ICalculator
+    {
+        public decimal Add(decimal a, decimal b) => a + b;
+
+        public decimal Subtract(decimal a, decimal b) => a - b;
+
+        public decimal Multiply(decimal a, decimal b) => a * b;
+
+        public decimal Divide(decimal a, decimal b)
+        {
+            if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+    }
+}
