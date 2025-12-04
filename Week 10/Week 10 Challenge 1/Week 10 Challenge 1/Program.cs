b84eec18ad77c1cfd9e4e118f4940c0b694c4fb4
Jeff <memphman2004@yahoo.com>
2025-12-04 16:05:44 -0500

    Add files via upload

---
+++ Week 10/Week 10 Challenge 1/Week 10 Challenge 1/Program.cs
@@ -0,0 +1,12 @@
+﻿public char FindTheDifference(string s, string t)
+{
+    int result = 0;
+
+    foreach (char c in s)
+        result ^= c;
+
+    foreach (char c in t)
+        result ^= c;
+
+    return (char)result;
+}
